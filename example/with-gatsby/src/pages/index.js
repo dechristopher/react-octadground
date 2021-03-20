f435a6d382fbc8b6eeeddb95d4a99c7af7753d17
Mike Upton <me@uptonm.dev>
2021-03-20 18:16:19 -0400

    v1.5.1-19

--- example/with-gatsby/src/pages/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-import Chessground from "react-chessground"
-import "react-chessground/dist/styles/chessground.css"
-
-const IndexPage = () => <Chessground />
-
-export default IndexPage
--- example/with-nextjs/pages/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react"
-import Chessground from "react-chessground"
-
-export default () => {
-  return <Chessground />
-}
